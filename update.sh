168091608ae1cda52ecf53c2e21e15ee59bfe3b1
elenaferr0 <elena.ferro@gmail.com>
2023-08-27 11:21:59 +0200

    eww config

--- update.sh
+++ update.sh
@@ -1,12 +1,8 @@
-echo "waybar"
-cp -r $HOME/.config/waybar/ .
 echo "nvim"
 cp $HOME/.config/nvim/init.lua ./nvim/init.lua
 cp $HOME/.config/nvim/lua/plugins.lua ./nvim/lua/plugins.lua
 echo "wofi"
 cp -r $HOME/.config/wofi/ .
-echo "dunst"
-cp -r $HOME/.config/dunst/ .
 echo "waybar"
 cp -r $HOME/.config/waybar/ .
 echo "hyprland"
@@ -15,9 +11,9 @@ echo "alacritty"
 cp -r $HOME/.config/alacritty/ .
 echo "zsh"
 cp $HOME/.zshrc .
-echo "swaylock"
-cp -r $HOME/.config/swaylock/ .
 echo "starship theme"
 cp ~/.config/starship.toml .
 echo "zathura"
 cp -r $HOME/.config/zathura/ .
+echo "gtklock"
+cp -r $HOME/.config/gtklock .
